c6a7b86f2aea60d6a04fc424bf8b080b6471f3b3
NatachaLng <73877685+NatachaLng@users.noreply.github.com>
2021-04-01 11:52:12 +0200

    add 3 characters rule

--- js/main.searchbar.js
+++ js/main.searchbar.js
@@ -2,11 +2,13 @@
 function searchBar (){
     let searchBar = document.querySelector("#mainSearchBar");
         searchBar.addEventListener('keydown', function (e) {
+            if (searchBar.value.length >= 3) {
                 document.getElementById('ustensil__tag').innerHTML = "";
                 document.getElementById("appliance__tag").innerHTML = "";
                 document.getElementById('ingredient__tag').innerHTML = "";
                 let searchString = e.target.value.toLowerCase();
                 recipeList.mainSearchBar(searchString);
+            }
                 if (13 == e.keyCode) {
                     searchBar.value = "";
                 }
